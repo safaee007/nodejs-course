c1b6fc2c05491354f843a73052d9cb50f69f6785
Saeed Safaee <Saeed.Safaee@ME1.iShiaCloud.net>
2019-12-29 13:46:23 +0330

    update

--- express.js
+++ express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var app = express();
+var port = 3000;
 
 app.get('/', (req, res) =>{
     res.send('home page');
@@ -9,6 +10,6 @@ app.get('/news', (req, res) =>{
     res.send('news list');
 });
 
-app.listen('3000', ()=>{
+app.listen(port, ()=>{
     console.log('server is runnig');
 })
