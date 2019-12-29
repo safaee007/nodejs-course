3b56fec6a1390bcfddbd3ec7b63fcde06f58ea74
Saeed Safaee <Saeed.Safaee@ME1.iShiaCloud.net>
2019-12-29 16:03:18 +0330

    hbs

--- express.js
+++ express.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const hbs = require('hbs');
 var app = express();
 var port = 3000;
 
-app.get('/', (req, res) =>{
-    res.send('home page');
+app.set('view engine', 'hbs');
+
+//middelwear
+app.use((req, res, next) =>{
+    let now = new Date().toString();
+    console.log(`time is ${now} and method ${req.path}`);
+    
+    next();
 });
 
+//site lock
+app.use((req, res, next) =>{
+    res.render('lockPage');
+});
+
+
+app.get('/', (req, res) =>{
+    res.send('home page');
+    //res.render('lockPage');
+});
 app.get('/news', (req, res) =>{
     res.send('news list');
 });
